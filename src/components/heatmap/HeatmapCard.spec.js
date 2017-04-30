6202a94634bf8fa9ef89c6278f15319231fbef26
wayofthepie <wayofthepie@gmail.com>
2017-04-30 12:57:54 +0100

    Add test for heatmap card container

--- src/components/heatmap/HeatmapCard.spec.js
+++ src/components/heatmap/HeatmapCard.spec.js
@@ -2,3 +2,38 @@
  *
  */
 'use strict';
+
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import HeatmapCard from './HeatmapCard';
+import {expect} from 'chai';
+import configureStore from 'redux-mock-store';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const middleware = [];
+const mockStore = configureStore(middleware);
+
+const CUSTOM_DATA = {
+  z: [[0, 10], [3, 4]],
+  x: ['Build Artifact', null],
+  y: ['UI', 'Titan'],
+  hoverinfo: 'text',
+  type: 'heatmap',
+  colorscale: []
+};
+
+const init = (data) => {
+  const props = {
+    data
+  };
+  return shallow(<HeatmapCard {...props} />);
+};
+
+describe('Heatmap Card initialization', () => {
+
+  it('should send a request for heatmap data', () => {
+    const store = mockStore({heatmap: {}});
+    mount(<MuiThemeProvider><HeatmapCard store={store} /></MuiThemeProvider>);
+    expect(store.getActions()).to.deep.equal([{type: 'REQUEST_HEATMAP'}])
+  })
+});
