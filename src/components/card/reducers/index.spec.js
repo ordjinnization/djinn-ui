bd0833461ac5b4f24febc264c479bfc91e2145d6
wayofthepie <wayofthepie@gmail.com>
2017-05-07 17:31:00 +0100

    Add reducer test for request.projects.success

--- src/components/card/reducers/index.spec.js
+++ src/components/card/reducers/index.spec.js
@@ -3,50 +3,64 @@
  */
 'use strict';
 import {expect} from 'chai';
-import {heatmap} from './';
+import {heatmap, projects} from './';
 
-describe('heatmap card reducer', () => {
+describe('heatmap card reducers', () => {
+  describe('heatmap reducer', () => {
 
-  it('should  have initial state of an empty object', () => {
-    const action = {
-      type: 'Unknown'
-    };
-    expect(heatmap(undefined, action)).to.deep.equal({});
+    it('should  have initial state of an empty object', () => {
+      const action = {
+        type: 'Unknown'
+      };
+      expect(heatmap(undefined, action)).to.deep.equal({});
+    });
+
+    it('should return given heatmap data for "request.heatmap.success"', () => {
+      const heatmapData = {test: 'test'};
+      const action = {
+        type: 'request.heatmap.success',
+        heatmapData
+      };
+      expect(heatmap(undefined, action)).to.equal(heatmapData);
+    });
+
+    it('should return error for "request.heatmap.failure"', () => {
+      const error = "Failure!!";
+      const action = {
+        type: 'request.heatmap.failure',
+        error
+      };
+      expect(heatmap(undefined, action)).to.equal(error);
+    });
+
+    it('should return given heatmap data for "request.heatmap.project.success"', () => {
+      const heatmapData = {test: 'test'};
+      const action = {
+        type: 'request.heatmap.project.success',
+        heatmapData
+      };
+      expect(heatmap(undefined, action)).to.equal(heatmapData);
+    });
+
+    it('should return error for "request.heatmap.project.failure"', () => {
+      const error = "Failure!!";
+      const action = {
+        type: 'request.heatmap.project.failure',
+        error
+      };
+      expect(heatmap(undefined, action)).to.equal(error);
+    });
+
   });
 
-  it('should return given heatmap data for "request.heatmap.success"', () => {
-    const heatmapData = {test: 'test'};
-    const action = {
-      type: 'request.heatmap.success',
-      heatmapData
-    };
-    expect(heatmap(undefined, action)).to.equal(heatmapData);
-  });
-
-  it('should return given heatmap data for "request.heatmap.project.success"', () => {
-    const heatmapData = {test: 'test'};
-    const action = {
-      type: 'request.heatmap.project.success',
-      heatmapData
-    };
-    expect(heatmap(undefined, action)).to.equal(heatmapData);
-  });
-
-  it('should return error for "request.heatmap.project.failure"', () => {
-    const error = "Failure!!";
-    const action = {
-      type: 'request.heatmap.project.failure',
-      error
-    };
-    expect(heatmap(undefined, action)).to.equal(error);
-  });
-
-  it('should return error for "request.heatmap.failure"', () => {
-    const error = "Failure!!";
-    const action = {
-      type: 'request.heatmap.failure',
-      error
-    };
-    expect(heatmap(undefined, action)).to.equal(error);
+  describe('projects reducer', () => {
+    it('should list of return projects when action type is "request.projects.success"', () => {
+      const projectList = {projects: ['shight', ' more-shight']};
+      const action = {
+        type: 'request.projects.success',
+        projects: projectList
+      };
+      expect(projects(undefined, action)).to.equal(projectList.projects);
+    });
   });
 });
