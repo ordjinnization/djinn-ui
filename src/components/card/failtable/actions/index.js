39769a7f52564217debcee1b391049161a442c18
wayofthepie <wayofthepie@gmail.com>
2017-05-09 18:35:10 +0100

    Add action for request latest succcess

--- src/components/card/failtable/actions/index.js
+++ src/components/card/failtable/actions/index.js
@@ -4,7 +4,15 @@
 'use strict';
 
 export const REQUEST_LATEST =  'request.failtable.latest';
+export const REQUEST_LATEST_SUCCESS =  'request.failtable.latest.success';
 
 export const requestLatest = () => {
   return { type: REQUEST_LATEST };
+};
+
+export const requestLatestSuccess = (latestResults) => {
+  return {
+    type: REQUEST_LATEST_SUCCESS,
+    latestResults
+  };
 };
--- src/components/card/failtable/actions/index.spec.js
+++ src/components/card/failtable/actions/index.spec.js
@@ -3,12 +3,22 @@
  */
 'use strict';
 import {expect} from 'chai';
-import {requestLatest} from './';
+import {requestLatest, requestLatestSuccess} from './';
 
 describe('fail table actions', () => {
   describe('request latest', () => {
-    it('should return action type for request latest', () => {
+    it('should return action type for "request.failtable.latest"', () => {
       expect(requestLatest()).to.be.deep.equal({type: 'request.failtable.latest'});
+    });
+  });
+
+  describe('request latest success', () => {
+    it('should return action for "request.failtable.latest.success" adn data', () => {
+      const expected = {
+        type: 'request.failtable.latest.success',
+        latestResults: ['results']
+      };
+      expect(requestLatestSuccess(['results'])).to.be.deep.equal(expected);
     });
   });
 });
